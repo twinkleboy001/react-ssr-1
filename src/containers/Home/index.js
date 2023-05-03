26975c3cd7223d47e03977147c08c0e56179556f
zhouhao <18190816904@163.com>
2023-05-03 17:40:10 +0800

    feat: update

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../components/Header";
 
 // 同构: 一套React代码，在服务器端执行一次，在客户端再执行一次
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <div>This is Dell Lee!</div>
       <button onClick={() => alert("click")}>click</button>
     </div>
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const Login = () => {
   return (
     <div>
+      <Header />
       <div>This is Login</div>
     </div>
   );
